7861a9f3c48936e170757122642469d56d34eef0
Ochirov Aiur <ochirovaur2@mail.ru>
2019-12-04 20:13:12 +0800

    another commit

--- src/js/index.js
+++ src/js/index.js
@@ -65,3 +65,60 @@ function init() {
   // Init TypeWriter
   new TypeWriter(txtElement, words, wait);
 }
+
+
+
+
+
+base = {
+  home: document.querySelector("#home"),
+  contact: document.querySelector("#contact"),
+  showcase: document.querySelector(".showcase"),
+  contactForm: document.querySelector(".contact-form")
+}
+
+showAndHideContactForm = (center, outFromView, percentCenter, percentOut) => {
+
+  center.style.right = `${percentCenter}%`;
+  outFromView.style.right = `${percentOut}%`;
+
+}
+
+toggleUnderline = (add, remove) => {
+  add.classList.add("current");
+  remove.classList.remove("current");
+}
+
+base.home.onclick = ()=> {
+
+  
+  toggleUnderline(base.home, base.contact);
+  
+  base.contactForm.style.transform = "scale(2) translateX(-75%) translateY(-75%) rotate(-28deg)";
+}
+
+base.contact.onclick = ()=> {
+  
+  toggleUnderline(base.contact, base.home);
+  
+
+  base.contactForm.style.transform = "scale(1) translateX(0) translateY(0) rotate(0)";
+  
+}
+
+
+
+$('#viewWorkBtn').on('click', function(e){
+          
+          if(this.hash !== "") {
+            
+            e.preventDefault();
+            
+            const hash = this.hash;
+            $('html, body').animate(
+              {
+                scrollTop: $(hash).offset().top
+              }, 700
+            );
+          }
+        });
