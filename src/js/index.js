f87f6430b53ca16166d083b74bdb753cc8cc2387
Ochirov Aiur <ochirovaur2@mail.ru>
2019-12-09 15:53:18 +0800

    Another change

--- src/js/index.js
+++ src/js/index.js
@@ -70,46 +70,46 @@ function init() {
 
 
 
-base = {
-  home: document.querySelector("#home"),
-  contact: document.querySelector("#contact"),
-  showcase: document.querySelector(".showcase"),
-  contactForm: document.querySelector(".contact-form"),
-  close: document.querySelector("#close")
-}
+// base = {
+//   home: document.querySelector("#home"),
+//   contact: document.querySelector("#contact"),
+//   showcase: document.querySelector(".showcase"),
+//   contactForm: document.querySelector(".contact-form"),
+//   close: document.querySelector("#close")
+// }
 
-showAndHideContactForm = (center, outFromView, percentCenter, percentOut) => {
+// showAndHideContactForm = (center, outFromView, percentCenter, percentOut) => {
 
-  center.style.right = `${percentCenter}%`;
-  outFromView.style.right = `${percentOut}%`;
+//   center.style.right = `${percentCenter}%`;
+//   outFromView.style.right = `${percentOut}%`;
 
-}
+// }
 
-toggleUnderline = (add, remove) => {
-  add.classList.add("current");
-  remove.classList.remove("current");
-}
-const closeContactForm = [base.close, base.home];
+// toggleUnderline = (add, remove) => {
+//   add.classList.add("current");
+//   remove.classList.remove("current");
+// }
+// const closeContactForm = [base.close, base.home];
 
-for(let i = 0; i < closeContactForm.length; i++) {
-  closeContactForm[i].onclick = ()=> {
+// for(let i = 0; i < closeContactForm.length; i++) {
+//   closeContactForm[i].onclick = ()=> {
 
     
-    toggleUnderline(base.home, base.contact);
+//     toggleUnderline(base.home, base.contact);
     
-    base.contactForm.style.transform = "scale(2) translateX(-75%) translateY(-75%) rotate(-28deg)";
-  }
-}
+//     base.contactForm.style.transform = "scale(2) translateX(-75%) translateY(-75%) rotate(-28deg)";
+//   }
+// }
 
 
-base.contact.onclick = ()=> {
+// base.contact.onclick = ()=> {
   
-  toggleUnderline(base.contact, base.home);
+//   toggleUnderline(base.contact, base.home);
   
 
-  base.contactForm.style.transform = "scale(1) translateX(0) translateY(0) rotate(0)";
+//   base.contactForm.style.transform = "scale(1) translateX(0) translateY(0) rotate(0)";
   
-}
+// }
 
 
 
